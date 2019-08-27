a6405d5a414be9a0db3722362fdb97ac76acde3d
milinsky <mail@milinsky.ru>
2019-08-27 16:39:41 +1000

    fix EventBus config starter task

--- src/EventBus.php
+++ src/EventBus.php
@@ -13,24 +13,19 @@ class EventBus
 {
     private $bus;
     
-    private $starterMember;
-    
-    private $action;
-    
-    private $members;
-    
     public function __construct(Bus $bus)
     {
         $this->bus = $bus;
         $this->bus->setMembersDirPath(Config::get('kernel.bus', 'members_dir_path'));
         $this->bus->setFiltersBinds(Config::get('kernel.bus', 'filters_binds'));
         $this->bus->setLayers(Config::get('kernel.bus', 'layers'));
+        $this->bus->setStartedTask(Config::get('kernel.bus', 'started_task'));
         $this->bus->init();
     }
     
     public function run(Request $request) : void
     {
-        $this->bus->run($this->starterMember, $this->action, $request);
+        $this->bus->run($request);
     }
     
     public function result(string $status, $data = null, string $timeout = '') : Result
